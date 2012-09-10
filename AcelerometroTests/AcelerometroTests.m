f3672d987b5cf106139e65d5034e5b85d0630fd2
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-09-10 10:44:59 -0300

    Initial Commit

---
+++ AcelerometroTests/AcelerometroTests.m
@@ -0,0 +1,32 @@
+//
+//  AcelerometroTests.m
+//  AcelerometroTests
+//
+//  Created by Rafael Brigagão Paulino on 10/09/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "AcelerometroTests.h"
+
+@implementation AcelerometroTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AcelerometroTests");
+}
+
+@end
